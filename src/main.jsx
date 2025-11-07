db8b60d440e8bad9b07e9b123802599718a71a40
Haris Naseer <130441679+harisnaseer24@users.noreply.github.com>
2025-11-07 18:18:54 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap ka import react side se bhi
-import { CartProvider } from "./components/cartContext";
+import { CartProvider } from "./components/CartContext";
 import { WishlistProvider } from "./components/WishlistContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
